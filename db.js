c9d7da1db7c5f0dfb566b806ad20f9cdf5a7089d
Tanmay Rane <tanmayrane3@gmail.com>
2026-02-21 21:37:21 +0530

    Add connection pool timeout settings

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const pool = new Pool({
   connectionString: process.env.SUPABASE_URI,
   ssl: { rejectUnauthorized: false },
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
 });
 
 module.exports = pool;
